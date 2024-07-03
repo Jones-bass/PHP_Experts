f9bc245afa2eaa4dc3be05dad776007feb8c7f8b
jones Silva <jonesbass.tb@gmail.com>
2024-07-02 21:48:56 -0300

    Estrutura Home

---
+++ PHP_API_PUBLIC/inc/header.php
@@ -0,0 +1,12 @@
+<?php 
+defined('CONTROL') or die('Acesso inválido');
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API dos Países</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
