b94bffcb4247237f249b99bae03890906c2159c7
jones Silva <jonesbass.tb@gmail.com>
2024-07-04 20:51:08 -0300

    Page Login

---
+++ PHP_LOGIN/inc/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Sistema de login simples com Base de Dados</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
---
+++ PHP_LOGIN/scripts/login.php
@@ -0,0 +1,22 @@
+
+
+<div class="login-form">
+    <div class="form-title">
+        Login
+    </div>
+    <form action="" method="post">
+        <div class="form-input">
+            <label for="text_usuario">Usuário</label>
+            <input type="text" name="text_usuario" required>
+        </div>
+        <div class="form-input">
+            <label for="text_senha">Senha</label>
+            <input type="password" name="text_senha" required>
+        </div>
+        <div class="form-button">
+            <input type="submit" name="submit" value="Entrar" class="btn">
+        </div>
+    </form>
+
+    
+</div>
